073419b0075241197ed468295839e665338a1840
golmal2466 <s.66.l.24bago@gmail.com>
2025-08-12 15:53:17 +0900

    Enhance main.tsx with comments for clarity on AuthProvider usage

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { AuthProvider } from './contexts/AuthContext.tsx'
+import { AuthProvider } from './contexts/AuthContext.tsx' // 私たちが作った「大司令塔」をインポート！
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    {/* AuthProviderという大司令塔で、アプリ全体を、丸ごと包む！ */}
     <AuthProvider>
       <App />
     </AuthProvider>
